2fa5e76263405d59be2b6d7671a93520fc59c5dd
githubmofo <109016237+githubmofo@users.noreply.github.com>
2026-07-08 16:04:55 +0530

    Time: 282 ms (45.5%), Space: 13.8 MB (83.15%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeStars(self, s):
+        stack = []
+
+        for ch in s:
+            if ch == '*':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(ch)
+
+        return ''.join(stack)
