918ef5fd1d19bd3f73b10780fecb5d5c3b517e30
Jenish Lad <109016237+githubmofo@users.noreply.github.com>
2026-08-04 13:41:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (36.73%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,6 +1,15 @@
 class Solution(object):
     def search(self, nums, target):
-        for i, val in enumerate(nums):
-            if val == target:
-                return i
+        n = len(nums)  
+        low = 0
+        high = n - 1
+
+        while low <= high:
+            mid = (low + high) // 2  
+            if nums[mid] == target:
+                return mid  
+            elif target > nums[mid]:
+                low = mid + 1  
+            else:
+                high = mid - 1  
         return -1
